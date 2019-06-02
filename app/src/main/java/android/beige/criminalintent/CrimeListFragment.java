e743621bb7134fb2adca75ec0f2cb915687471e2
matt <mholford@protonmail.com>
2019-06-02 13:26:16 -0700

    Chapter 7.1

--- app/src/main/java/android/beige/criminalintent/CrimeListFragment.java
+++ app/src/main/java/android/beige/criminalintent/CrimeListFragment.java
@@ -7,12 +7,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class CrimeListFragment extends Fragment {
+    private static final int IS_BENIGN_CRIME = 0;
+    private static final int IS_SERIOUS_CRIME = 1;
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
 
@@ -33,7 +36,7 @@ public class CrimeListFragment extends Fragment {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
+        if (mAdapter == null) mAdapter = new CrimeAdapter(crimes);
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
@@ -47,7 +50,18 @@ public class CrimeListFragment extends Fragment {
         @Override
         public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            return new CrimeHolder(layoutInflater, parent);
+            if (viewType == IS_BENIGN_CRIME) return new CrimeHolderBenign(layoutInflater, parent);
+            else return new CrimeHolderSerious(layoutInflater, parent);
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            Crime mCrime = mCrimes.get(position);
+            int viewType;
+
+            if (!mCrime.getRequiresPolice()) viewType = IS_BENIGN_CRIME;
+            else viewType = IS_SERIOUS_CRIME;
+            return viewType;
         }
 
         @Override
@@ -62,14 +76,14 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder
+    private abstract class CrimeHolder extends RecyclerView.ViewHolder
             implements View.OnClickListener {
         private Crime mCrime;
         private TextView mTitleTextView;
         private TextView mDateTextView;
 
-        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.item_crimelist, parent, false));
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent, int layout) {
+            super(inflater.inflate(layout, parent, false));
             itemView.setOnClickListener(this);
 
             mTitleTextView = itemView.findViewById(R.id.tv_crimelist_title);
@@ -86,6 +100,37 @@ public class CrimeListFragment extends Fragment {
         public void onClick(View view) {
             Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!",
                     Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class CrimeHolderBenign extends CrimeHolder {
+
+        CrimeHolderBenign(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater, parent, R.layout.item_crimelist_benign);
+        }
+    }
+
+    private class CrimeHolderSerious extends CrimeHolder {
+        private Button mBtnContactPolice;
+
+        CrimeHolderSerious(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater, parent, R.layout.item_crimelist_serious);
+        }
+
+        @Override
+        public void bind(final Crime crime) {
+            super.bind(crime);
+
+            mBtnContactPolice = itemView.findViewById(R.id.btn_crimelist);
+            mBtnContactPolice.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String policeToast = "srs crime: "
+                            + crime.getTitle();
+
+                    Toast.makeText(getActivity(), policeToast, Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
