0ead178cc599ed4e06990814f0db11674cd315b2
matt <mholford@protonmail.com>
2019-06-03 00:52:21 -0700

    Chapter 11

--- app/src/main/java/android/beige/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/android/beige/criminalintent/CrimePagerActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +19,8 @@ public class CrimePagerActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+    private Button mBtnFirst;
+    private Button mBtnLast;
 
     public static Intent newIntent(Context pkgcontext, UUID crimeID) {
         Intent intent = new Intent(pkgcontext, CrimePagerActivity.class);
@@ -27,13 +31,13 @@ public class CrimePagerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_crime_pager);
-
         UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
-
-        mViewPager = findViewById(R.id.viewpager_crime);
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
+
+        mViewPager = findViewById(R.id.viewpager_crime);
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int position) {
@@ -53,5 +57,46 @@ public class CrimePagerActivity extends AppCompatActivity {
                 break;
             }
         }
+
+        mBtnFirst = findViewById(R.id.btn_pager_first);
+        mBtnFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+
+        mBtnLast = findViewById(R.id.btn_pager_last);
+        mBtnLast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mViewPager.getAdapter().getCount() - 1);
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPx) {
+                int currentPage = mViewPager.getCurrentItem();
+                int lastPage = mViewPager.getAdapter().getCount() - 1;
+
+                if (currentPage == 0) mBtnFirst.setEnabled(false);
+                else {
+                    mBtnFirst.setEnabled(true);
+                    if (currentPage == lastPage) mBtnLast.setEnabled(false);
+                    else mBtnLast.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
     }
 }
